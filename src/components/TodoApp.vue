24291efac292bee93b48e4274cb01857f8e1e391
MuhammadHanifNurcholis <muhammadhanifnurcholis@student.uir.ac.id>
2025-05-05 18:13:43 +0700

    Setup Dasar dan Struktur Template

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+const newItem = ref("");
+const newPrice = ref(0);
+const items = ref([]);
+</script>
+
+<template>
+  <div class="app-background">
+    <div class="app-container">
+      <h1 class="title">🛒 Daftar Belanja Pribadi</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
