80b94a89d7729bd3f0217ff61da77989aeb1698a
MuhammadHanifNurcholis <muhammadhanifnurcholis@student.uir.ac.id>
2025-05-05 18:06:57 +0700

    inisialisasi project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TodoApp from "./components/Todo-app.vue";
+</script>
+
+<template>
+  <TodoApp />
+
+</template>
+
+<style scoped>
+
+</style>
